03e57f6a252f6cb1acc44c21ee36a9f6d9afb0c0
JT <raikiri24@gmail.com>
2023-07-23 20:21:43 -0400

    updates routes

--- controllers/user_routes.js
+++ controllers/user_routes.js
@@ -1,85 +1,91 @@
 const router = require('express').Router();
 const User = require('../models/User');
 
+//login existing user
 router.post('/login', async (req, res) => {
-    try {
-        const formIdentifier = req.body.email;
-        const formPassword = req.body.password;
+  try {
+    const formIdentifier = req.body.email;
+    const formPassword = req.body.password;
 
-        const isEmailFormat = /\S+@\S+\.\S+/.test(formIdentifier);
-        let user;
-
-        if (isEmailFormat) {
-            user = await User.findOne({
-                where: {
-                    email: formIdentifier
-                }
-            });
-        } else {
-            user = await User.findOne({
-                where: {
-                    username: formIdentifier
-                }
-            });
+    const isEmailFormat = /\S+@\S+\.\S+/.test(formIdentifier);
+    let user;
+    //confirm email or username
+    if (isEmailFormat) {
+      user = await User.findOne({
+        where: {
+          email: formIdentifier
         }
-        if (!user) {
-            console.log('User Does Not Exist')
-            return res.redirect('/register');
+      });
+    } else {
+      user = await User.findOne({
+        where: {
+          username: formIdentifier
         }
-
-        const isValidPass = await user.validatePass(formPassword);
-
-        if (!isValidPass) {
-            console.log('Invalid Password')
-            return res.send({ error: 'Incorrect Password' })
-        }
-
-        req.session.user_id = user.id;
-        res.redirect('/dashboard');
-    } catch (err) {
-        console.error(err);
-        res.status(500).send('Internal Server Error');
-
+      });
     }
+    if (!user) {
+      console.log('User Does Not Exist')
+      return res.redirect('/register');
+    }
+    //confirm password
+    const isValidPass = await user.validatePass(formPassword);
+
+    if (!isValidPass) {
+      console.log('Invalid Password')
+      return res.send({ error: 'Incorrect Password' })
+    }
+
+    req.session.user_id = user.id;
+    res.redirect('/dashboard');
+  } catch (err) {
+    console.error(err);
+    res.status(500).send('Internal Server Error');
+
+  }
 });
 
+//register new user
 router.post('/register', async (req, res) => {
-    try {
-      const password = req.body.password;
-      const verifyPassword = req.body.verifyPassword;
-  
-      console.log("Password:", req.body.password);
-      console.log("Verify Password:", req.body.verifyPassword);
-  
-      if (password !== verifyPassword) {
-        return res.redirect('/register');
-      };
+  try {
+    const password = req.body.password;
+    const verifyPassword = req.body.verifyPassword;
 
-      // Check if the email is already taken
-      const existingEmail = await User.findOne({ where: { email: req.body.email } });
-      if (existingEmail) {
-        return res.redirect('/register');
-      }
-  
-      // Check if the username is already taken
-      const existingUsername = await User.findOne({ where: { username: req.body.username } });
-      if (existingUsername) {
-        return res.redirect('/register');
-      }
-  
-      // Passwords match, and email/username are available, proceed with user creation
-      const newUser = await User.create(req.body);
-      req.session.user_id = newUser.id;
-      console.log('Created ID');
-      res.redirect('/mood');
-  
-    } catch (err) {
-      // Log the error message to the console
-      console.error(err);
-  
-      // Handle other errors
-      return res.status(500).json('Internal Server Error');
+    console.log("Password:", req.body.password);
+    console.log("Verify Password:", req.body.verifyPassword);
+
+    if (password !== verifyPassword) {
+      return res.redirect('/register');
+    };
+
+    // verify email is available for use
+    const existingEmail = await User.findOne({ where: { email: req.body.email } });
+    if (existingEmail) {
+      return res.redirect('/register');
     }
-  });
+
+    // verify username is available for use
+    const existingUsername = await User.findOne({ where: { username: req.body.username } });
+    if (existingUsername) {
+      return res.redirect('/register');
+    }
+
+    //Proceed with user creation
+    const newUser = await User.create(req.body);
+    req.session.user_id = newUser.id;
+    console.log('Created ID');
+    res.redirect('/dashboard');
+
+  } catch (err) {
+    console.error(err);
+    return res.status(500).json('Internal Server Error');
+  }
+});
+
+//log out user
+router.get('/logout', (req, res) => {
+  req.session.destroy();
+  res.clearCookie('user_id')
+  res.redirect('/');
+});
 
 module.exports = router;
--- controllers/view_routes.js
+++ controllers/view_routes.js
@@ -1,3 +1,36 @@
 const router = require('express').Router();
+const User = require('../models/User');
+const Dashboard = require('../models/Dashboard');
+const bcrypt = require('bcrypt')
+
+// Homepage
+router.get('/', (req, res) => {
+
+    res.render('index', {
+        isHome: true,
+        isLoggedIn: req.session.user_id
+    });
+});
+// Login page
+router.get('/login', (req, res) => {
+    if (req.session.user_id) return res.redirect('/mood');
+
+    res.render('login', {
+        isLogin: true
+    });
+});
+// Register Page
+router.get('/register', (req, res) => {
+    res.render('register', {
+        isRegister: true
+    });
+});
+
+// Dashboard Page
+router.get('/dashboard', (req, res) => {
+    if (!req.session.user_id) return res.redirect('/login');
+
+    res.render('dashboard');
+});
 
 module.exports = router;
